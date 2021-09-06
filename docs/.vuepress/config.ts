ca830940502984626dc43f23d9a8a3f77c02bdde
xfy520 <longzinziyan@gmail.com>
2021-09-06 17:22:40 +0800

    文档项目初始化

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,49 @@
+import { defineUserConfig } from '@vuepress/cli';
+import { UserConfig } from 'vite';
+import { Options } from '@vitejs/plugin-vue';
+import type { DefaultThemeOptions } from '@vuepress/theme-default';
+
+export default defineUserConfig<DefaultThemeOptions, {
+  viteOptions: UserConfig,
+  vuePluginOptions: Options
+}>({
+  base: '/',
+  lang: 'zh-CN',
+  title: '技术杂谈',
+  description: '欢迎来到闲聊世界',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }]
+  ],
+  bundlerConfig: {},
+  plugins: [
+    [
+      '@vuepress/plugin-search',
+      {
+        placeholder: '搜索',
+        hotKeys: ['s', '/']
+      }
+    ],
+    [
+      '@vuepress/plugin-prismjs',
+      {
+        preloadLanguages: ['java', 'css', 'javascript', 'typescript', 'html', 'json']
+      }
+    ]
+  ],
+  themeConfig: {
+    home: '/',
+    notFound: ['访问出错了!'],
+    backToHome: '返回首页',
+    themePlugins: {
+      prismjs: true
+    }
+  },
+  bundler: '@vuepress/vite',
+  dest: `${__dirname}../../../.dist`,
+  temp: `${__dirname}../../../.temp`,
+  cache: `${__dirname}../../../.cache`,
+  port: 8081,
+  open: false,
+})
---
+++ docs/README.md
@@ -0,0 +1,31 @@
+---
+home: true
+title: 首页
+actions:
+  - text: 开始阅读
+    link: /guide/getting-started.html
+    type: primary
+  - text: 文档结构
+    link: /guide/
+    type: secondary
+features:
+  - title: Css3
+    details: 记录平时前端开发过程中一些样式使用方式，如Animation、Transition、Transform等一些样式的编写。
+  - title: JavaScript
+    details: 记录 JavaScript 在使用的过程中遇到的一些比较偏的问题解决、平时编写的小插件等。
+  - title: Java
+    details: Java8新特性、MyBatis-Plus、Spring Boot、JcasBin等相关使用问题。
+  - title: Vue3.0
+    details: 个人开源 Vue3.0 生态组件、Vue3.0使用过程中出现的问题等。
+  - title: Vue3.0 UI 组件库
+    details: 如 Ant Design Vue、ElementPlus 等支持Vue3.0系列UI组件使用问题。
+  - title: 其他
+    details: Vite2.0、谷歌插件开发、Docker、Traefik2.0等使用问题。
+footer: MIT Licensed | Copyright © 2021-present longzinziyan@gmail.com
+---
+
+### 开启你的快乐之旅
+
+```ts
+console.log('Hello World');
+```
