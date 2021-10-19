d2a35dc2d28dfdcd8af537ed219cd9f49fe71713
xfy520 <longzinziyan@gmail.com>
2021-10-19 23:25:00 +0800

    add prismjs plugin

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -26,8 +26,9 @@ const plugins: PluginConfig[] = [
   [
     'vuepress-plugin-prismjs-next',
     {
-      languages: ['java', 'css', 'javascript', 'typescript', 'html', 'json', 'shell', 'yaml', 'diff'],
-      plugins: ['inline-color', 'autolinker', 'data-uri-highlight', 'diff-highlight', 'treeview', 'highlight-keywords', 'line-numbers'],
+      languages: ['less', 'css', 'javascript', 'sass', 'html', 'scss', 'stylus', 'yaml', 'diff'],
+      plugins: ['inline-color', 'autolinker', 'data-uri-highlight', 'diff-highlight', 'treeview', 'show-invisibles', 'match-braces',
+      'highlight-keywords', 'line-numbers', 'line-highlight', 'toolbar', 'show-language', 'copy-to-clipboard', 'download-button'],
       theme: "dark",
     },
   ],
